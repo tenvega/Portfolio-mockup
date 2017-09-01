bd466dd40d8619ee11d1aac345d2da7043751c9e
Enoc <etorra@gmail.com>
2017-08-31 21:40:09 -0400

    styling in process

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import '../styles/App.css';
+import BG from '../images/BG.jpg';
+import Arturia from '../images/Arturia.jpg';
+import enoc from '../images/enoc.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-      <header>
 
-      </header>
-        <div>
+        <div >
 
           <h1>React Portfolio App</h1>
+          <img id="BG" src={BG} alt="" />
+          <img id="Arturia" src={Arturia} alt="" />
+          <img id="enoc" src={enoc} alt="" />
+
+
 
         </div>
 
